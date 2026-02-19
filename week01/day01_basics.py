aeab86d7d85bf7546fc42f58eb4841d9246f862a
zpathan <zpathan@student.ubc.ca>
2026-02-19 15:28:01 -0800

    update

--- week01/day01_basics.py
+++ week01/day01_basics.py
@@ -16,10 +16,27 @@ def std(xs):
     var = sum((x - mu) ** 2 for x in xs) / len(xs)
     return math.sqrt(var)
 
+print(std([10, 10, 10]))
+print(std([-2, 0, 2]))
+
+
+
+
 def minmax_scale(xs):
     # Scale to [0, 1]. If all values equal, return all 0.0
-    # TODO
-    pass
+   if len(xs) == 0:  # len returns number of items 
+        raise ValueError("minmax_scale() requires atleast one value") 
+   lo = min(xs)
+   hi = max(xs)
+   if hi == lo:
+       return [0.0 for _ in xs]      # you use _ when you dont care about the variable
+                                     # and only used for looping
+   return [(x - lo)/(hi - lo) for x in xs] 
+
+print(minmax_scale([10, 10, 10]))
+print(minmax_scale([-2, 0, 2]))
+
+   
 
 # ---- tests ----
 tests = [
