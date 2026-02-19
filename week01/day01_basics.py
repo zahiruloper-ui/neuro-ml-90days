855787f7d78960ef035250b8bd8837b5910b1128
zahiruloper-ui <zahiruloper@gmail.com>
2026-02-19 14:55:48 -0800

    add stats function skeleton

--- week01/day01_basics.py
+++ week01/day01_basics.py
@@ -1,1 +1,30 @@
 print("Day 1 ready")
+import math  # for math.sqrt [web:134]
+
+def mean(xs):
+    # TODO
+    pass
+
+def std(xs):
+    # Population standard deviation: sqrt(mean((x - mu)^2))
+    # TODO
+    pass
+
+def minmax_scale(xs):
+    # Scale to [0, 1]. If all values equal, return all 0.0
+    # TODO
+    pass
+
+# ---- tests ----
+tests = [
+    [1, 2, 3, 4, 5],
+    [10, 10, 10],
+    [-2, 0, 2],
+]
+
+for xs in tests:
+    print("xs =", xs)
+    print("mean =", mean(xs))
+    print("std  =", std(xs))
+    print("mm   =", minmax_scale(xs))
+    print()
